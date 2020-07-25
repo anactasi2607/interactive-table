529d88816ff695725384a20aa36641d604949b6e
anactasi2607 <anactasi2607@yandex.ru>
2020-07-25 11:11:56 +0300

    Реализована логика добавления пользователя в таблицу

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import Loader from "./Loader/Loader";
-import Table from "./Table/Table";
+import Loader from "./Loader/Loader.js";
+import Table from "./Table/Table.js";
+import Form from "./Form/Form.js";
 
 class App extends Component {
   state = {
     isLoading: true,
     data: [],
+    newUser: {
+      id: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+    },
   };
 
   async componentDidMount() {
@@ -21,10 +29,47 @@ class App extends Component {
     });
   }
 
+  onChangeInput = (evt) => {
+    this.setState({
+      newUser: {
+        ...this.state.newUser,
+        [evt.target.id]: evt.target.value,
+      },
+    });
+  };
+
+  submitHandler = (evt) => {
+    evt.preventDefault();
+    console.log(this.state.newUser);
+    const { newUser, data } = this.state;
+
+    this.setState({
+      data: [newUser, ...data],
+      newUser: {
+        id: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+      },
+    });
+  };
+
   render() {
     return (
       <div className="container">
-        {this.state.isLoading ? <Loader /> : <Table data={this.state.data} />}
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <Form
+              submitHandler={this.submitHandler}
+              onChangeInput={this.onChangeInput}
+              newUser={this.state.newUser}
+            />
+            <Table data={this.state.data} />
+          </React.Fragment>
+        )}
       </div>
     );
   }
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,34 +1,30 @@
 import React from "react";
 import "./Table.css";
-import Form from "./../Form/Form.js";
 
 function Table(props) {
   return (
-    <React.Fragment>
-      <Form />
-      <table className="table">
-        <thead className="table-thead">
-          <tr className="table-tr">
-            <th className="table-th">ID</th>
-            <th className="table-th">First Name</th>
-            <th className="table-th">Last Name</th>
-            <th className="table-th">E-mail</th>
-            <th className="table-th">Phone</th>
+    <table className="table">
+      <thead className="table-thead">
+        <tr className="table-tr">
+          <th className="table-th">ID</th>
+          <th className="table-th">First Name</th>
+          <th className="table-th">Last Name</th>
+          <th className="table-th">E-mail</th>
+          <th className="table-th">Phone</th>
+        </tr>
+      </thead>
+      <tbody className="table-tbody">
+        {props.data.map((item) => (
+          <tr className="table-tr" key={item.id + item.phone}>
+            <td className="table-td">{item.id}</td>
+            <td className="table-td">{item.firstName}</td>
+            <td className="table-td">{item.lastName}</td>
+            <td className="table-td">{item.email}</td>
+            <td className="table-td">{item.phone}</td>
           </tr>
-        </thead>
-        <tbody className="table-tbody">
-          {props.data.map((item) => (
-            <tr className="table-tr" key={item.id + item.phone}>
-              <td className="table-td">{item.id}</td>
-              <td className="table-td">{item.firstName}</td>
-              <td className="table-td">{item.lastName}</td>
-              <td className="table-td">{item.email}</td>
-              <td className="table-td">{item.phone}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </React.Fragment>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
