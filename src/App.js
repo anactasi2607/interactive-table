9e017d0acbdd862140593323ad0cef5401592893
anactasi2607 <anactasi2607@yandex.ru>
2020-07-27 18:02:14 +0300

    Добавлена сортировка

--- src/App.js
+++ src/App.js
@@ -4,11 +4,58 @@ import Loader from "./Loader/Loader.js";
 import Table from "./Table/Table.js";
 import Form from "./Form/Form.js";
 import DetailInfo from "./DetailInfo/DetailInfo.js";
+import _ from "lodash";
 
 class App extends Component {
   state = {
     isLoading: true,
     data: [],
+    sort: "asc",
+    sortField: "",
+    mokDate: [
+      {
+        id: 101,
+        firstName: "Sue",
+        lastName: "Corson",
+        email: "DWhalley@in.gov",
+        phone: "(612)211-6296",
+        address: {
+          streetAddress: "9792 Mattis Ct",
+          city: "Waukesha",
+          state: "WI",
+          zip: "22178",
+        },
+        description: "et lacus magna dolor...",
+      },
+      {
+        id: 102,
+        firstName: "Иван",
+        lastName: "Петров",
+        email: "Ahnalley@in.gov",
+        phone: "(912)211-6200",
+        address: {
+          streetAddress: "улица Советская",
+          city: "порплоsha",
+          state: "WI",
+          zip: "22178",
+        },
+        description: "et lacus magna dolor...",
+      },
+      {
+        id: 103,
+        firstName: "Марья",
+        lastName: "Сидорова",
+        email: "Msdfhalley@in.gov",
+        phone: "(512)211-5533",
+        address: {
+          streetAddress: "улица Кирова",
+          city: "Северодвинск",
+          state: "WI",
+          zip: "22178",
+        },
+        description: "et lacus magna dolor...",
+      },
+    ],
     newUser: {
       id: "",
       firstName: "",
@@ -42,7 +89,6 @@ class App extends Component {
 
   submitHandler = (evt) => {
     evt.preventDefault();
-    console.log(this.state.newUser);
     const { newUser, data } = this.state;
 
     this.setState({
@@ -64,9 +110,19 @@ class App extends Component {
   };
 
   onClickClose = () => {
-    console.log("закрываю попап");
     this.setState({
       selectedRow: null,
+    });
+  };
+
+  onSort = (sortField) => {
+    const cloneData = this.state.data.concat();
+    const sortType = this.state.sort === "asc" ? "desc" : "asc";
+    const orderedData = _.orderBy(cloneData, sortField, sortType);
+    this.setState({
+      data: orderedData,
+      sort: sortType,
+      sortField,
     });
   };
 
@@ -82,7 +138,13 @@ class App extends Component {
               onChangeInput={this.onChangeInput}
               newUser={this.state.newUser}
             />
-            <Table data={this.state.data} onClickRow={this.onClickRow} />
+            <Table
+              data={this.state.data}
+              onClickRow={this.onClickRow}
+              onSort={this.onSort}
+              sort={this.state.sort}
+              sortField={this.state.sortField}
+            />
             {this.state.selectedRow ? (
               <DetailInfo
                 user={this.state.selectedRow}
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -6,11 +6,56 @@ function Table(props) {
     <table className="table">
       <thead className="table-thead">
         <tr className="table-tr">
-          <th className="table-th">ID</th>
-          <th className="table-th">First Name</th>
-          <th className="table-th">Last Name</th>
-          <th className="table-th">E-mail</th>
-          <th className="table-th">Phone</th>
+          <th
+            className="table-th"
+            onClick={props.onSort.bind(null, "id")}
+            title="Нажмите для сортировки"
+          >
+            ID{" "}
+            {props.sortField === "id" ? (
+              <span className={props.sort}></span>
+            ) : null}
+          </th>
+          <th
+            className="table-th"
+            onClick={props.onSort.bind(null, "firstName")}
+            title="Нажмите для сортировки"
+          >
+            First Name
+            {props.sortField === "firstName" ? (
+              <span className={props.sort}></span>
+            ) : null}
+          </th>
+          <th
+            className="table-th"
+            onClick={props.onSort.bind(null, "lastName")}
+            title="Нажмите для сортировки"
+          >
+            Last Name
+            {props.sortField === "lastName" ? (
+              <span className={props.sort}></span>
+            ) : null}
+          </th>
+          <th
+            className="table-th"
+            onClick={props.onSort.bind(null, "email")}
+            title="Нажмите для сортировки"
+          >
+            E-mail
+            {props.sortField === "email" ? (
+              <span className={props.sort}></span>
+            ) : null}
+          </th>
+          <th
+            className="table-th"
+            onClick={props.onSort.bind(null, "phone")}
+            title="Нажмите для сортировки"
+          >
+            Phone
+            {props.sortField === "phone" ? (
+              <span className={props.sort}></span>
+            ) : null}
+          </th>
         </tr>
       </thead>
       <tbody className="table-tbody">
@@ -19,6 +64,7 @@ function Table(props) {
             className="table-tr"
             key={item.id + item.phone}
             onClick={props.onClickRow.bind(null, item)}
+            title="Нажмите для получения подробной информации"
           >
             <td className="table-td">{item.id}</td>
             <td className="table-td">{item.firstName}</td>
